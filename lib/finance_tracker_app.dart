986049c976b9199eb850b409c80166aaa9c784a1
youssefkhaled <youssefwahdan12@gmail.com>
2025-11-10 21:39:19 +0200

    Project Created

---
+++ lib/finance_tracker_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FinanceTrackerApp extends StatelessWidget {
+  const FinanceTrackerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:finance_tracker/finance_tracker_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FinanceTrackerApp());
+}
