f9989da7693290782ad41b1ce71bc6778a25b8ca
youssefkhaled <youssefwahdan12@gmail.com>
2025-11-11 22:52:51 +0200

    Second setup: Created font weight helper file Created file for app text styles

---
+++ lib/core/theming/text_styles.dart
@@ -0,0 +1,76 @@
+import 'package:finance_tracker/core/theming/app_colors.dart';
+import 'package:finance_tracker/core/theming/font_weight_helper.dart';
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static TextStyle font36WhiteBlackWeight = TextStyle(
+    fontSize: 36,
+    color: AppColors.textLight,
+    fontWeight: FontWeightHelper.black,
+  );
+  static TextStyle font32WhiteBold = TextStyle(
+    fontSize: 32,
+    color: AppColors.textLight,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font28WhiteBold = TextStyle(
+    fontSize: 28,
+    color: AppColors.textLight,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font24WhiteBold = TextStyle(
+    fontSize: 24,
+    color: AppColors.textLight,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font22WhiteBold = TextStyle(
+    fontSize: 22,
+    color: AppColors.textLight,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font18WhiteBold = TextStyle(
+    fontSize: 18,
+    color: AppColors.textLight,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font16WhiteBold = TextStyle(
+    fontSize: 16,
+    color: AppColors.textLight,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font14WhiteBold = TextStyle(
+    fontSize: 14,
+    color: AppColors.textLight,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font13WhiteBold = TextStyle(
+    fontSize: 13,
+    color: AppColors.textLight,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font16WhiteRegular = TextStyle(
+    fontSize: 16,
+    color: AppColors.textLight,
+    fontWeight: FontWeightHelper.regular,
+  );
+  static TextStyle font14WhiteRegular = TextStyle(
+    fontSize: 14,
+    color: AppColors.textLight,
+    fontWeight: FontWeightHelper.regular,
+  );
+  static TextStyle font16WhiteMedium = TextStyle(
+    fontSize: 16,
+    color: AppColors.textLight,
+    fontWeight: FontWeightHelper.medium,
+  );
+  static TextStyle font14WhiteMedium = TextStyle(
+    fontSize: 14,
+    color: AppColors.textLight,
+    fontWeight: FontWeightHelper.medium,
+  );
+  static TextStyle font12WhiteMedium = TextStyle(
+    fontSize: 12,
+    color: AppColors.textLight,
+    fontWeight: FontWeightHelper.medium,
+  );
+}
